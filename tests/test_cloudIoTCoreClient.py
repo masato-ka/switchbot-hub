981da6908b30ff31ce7e9d8e5d544d6c8f401e54
masato-ka <jp6uzv@gmail.com>
2019-01-04 18:46:41 +0900

    Update config.yml

---
+++ tests/test_cloudIoTCoreClient.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+
+
+class TestCloudIoTCoreClient(TestCase):
+
+    def test_connect(self):
+        pass
+
+    def test_setObserver(self):
+        pass
+
+    def test_publish_telemetry(self):
+        pass
+
+    def test_publish_status(self):
+        pass
+
+    def test__error_str(self):
+        pass
+
+    def test__on_connect(self):
+        pass
+
+    def test__on_disconnect(self):
+        pass
+
+    def test__on_publish(self):
+        pass
+
+    def test__on_message(self):
+        pass
+
+    def test__get_client(self):
+        pass
+
+    def test__create_jwt(self):
+        pass
